841c15155b4a02ef065e7559312d7f00c5ab125e
S.B.A <125323075+Diego-AArturo@users.noreply.github.com>
2025-05-23 00:08:52 -0500

    Update api.js

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,5 +1,5 @@
 export async function fetchSession() {
-    const res = await fetch("/session");
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/session`);
     const data = await res.json();
     return data.client_secret.value;
 }
